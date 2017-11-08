50319dd17c7125e81136599047a6a732aa9b954c
Payscout Inc <victor@payscout.com>
2017-11-08 15:34:12 +0200

    Create void.js

---
+++ void.js
@@ -0,0 +1,33 @@
+var qs = require("querystring");
+var http = require("https");
+
+var options = {
+  "method": "POST",
+  "hostname": "gatewaystaging.payscout.com",
+  "port": null,
+  "path": "/api/process",
+  "headers": {
+    "content-type": "application/x-www-form-urlencoded",
+    "cache-control": "no-cache"
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.write(qs.stringify({ client_username: '{yourUsername}',
+  client_password: '{yourPassword}',
+  client_token: 'token',
+  processing_type: 'VOID',
+  original_transaction_id: 'A000177MEKX' }));
+req.end(); 
